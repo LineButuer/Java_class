753ef0fff6e2e55532f99e0bd64f3cd91f93fc62
linebutcher <dlxodid1115@gmail.com>
2023-11-30 15:46:48 +0900

    사칙연산

---
+++ src/ch02_variables/Ex01_ArithmeticOperator.java
@@ -0,0 +1,23 @@
+package ch02_variables;
+
+public class Ex01_ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// 산술연산자
+		/**
+		 * num1, num2 두 개의 정수형 변수를 선언하고 값은 마음대로
+		 * 두 변수의 +, -, *, /, % 연산결과를 print()를 활용하여 출력해봅시다.
+		 * 출력방법
+		 * System.out.println(num1 + num2)
+		 */
+		int num1 = 29, num2 = 30;
+		
+		System.out.println(num1+num2 + "->" + "덧셈" );
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
+	
+	}
+
+}
