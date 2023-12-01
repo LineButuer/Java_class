957097e533ccc90eea8749174e1f556c4bdc26bd
linebutcher <dlxodid1115@gmail.com>
2023-12-01 10:03:17 +0900

    논리연산자2

--- src/ch02_variables/Ex04_LogicalOperator.java
+++ src/ch02_variables/Ex04_LogicalOperator.java
@@ -26,14 +26,27 @@ public class Ex04_LogicalOperator {
 		// result1 && result2 결과를 result3 에 저장한 뒤 
 		// result3의 값을 출력
 		
-		int num1 =10, num2= 20;
+		int num1 =11, num2= 20;
 		
-		boolean result1 = num1 > num2;
-		boolean result2 = num1 != num2;
+		boolean result1 = (num1 > num2);
 		
-		boolean result3 = result1 && result2;
+		boolean result2 = (num1 != num2);
+		
+		boolean result3 = (result1 && result2);
 		
 		System.out.println(result3);
+		
+		boolean result4 = (num1 > num2) && (num1 != num2);
+		System.out.println(result4);
+		
+		System.out.println(bool1); //t
+		System.out.println(!bool1); //f
+		System.out.println(bool1 && bool2); //t
+		System.out.println(!(bool1 && bool2)); //f
+		
+		System.out.println("NOT 연산 수행 전 : " + bool2);
+		bool2=!bool2;
+		System.out.println("NOT 연산 수행 후 : " + bool2);
 		
 	
  }
---
+++ src/ch02_variables/Ex05_IncreaseDecreaseOperaor.java
@@ -0,0 +1,13 @@
+package ch02_variables;
+
+public class Ex05_IncreaseDecreaseOperaor {
+
+	public static void main(String[] args) {
+		// 증감연산자
+		int num1=10, num2=0, num3=0;
+		System.out.println("num1: " + num1 + "num2: " + num2 + "num3: "+num3);
+		num2 = ++num1;
+		System.out.println("num1: " + num1 + "num2: " + num2 + "num3: "+num3);
+	}
+
+}
