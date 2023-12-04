5146e780bb816baf23b813515a7a8d853882864d
linebutcher <dlxodid1115@gmail.com>
2023-12-04 11:21:40 +0900

    20231204(4)

--- src/ch04_if/Ex07_IfExamle.java
+++ src/ch04_if/Ex07_IfExamle.java
@@ -6,39 +6,35 @@ public class Ex07_IfExamle {
 
 	public static void main(String[] args) {
 		/**
-		 * 성적 출력 예제 응용
-		 * 입력값 : 학년(year), 점수(score)
-		 * 처리
-		 * 	- 1~3학년은 60점 이상이면 합격
-		 *  - 4학년은 70점 이상이어야 합격
+		 * 성적 출력 예제 응용 입력값 : 학년(year), 점수(score) 처리 - 1~3학년은 60점 이상이면 합격 - 4학년은 70점
+		 * 이상이어야 합격
 		 */
 
-		int score =0;
+		int score = 0;
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("학년을 입력하세요.");
 		int year1 = scanner.nextInt();
 		System.out.println("점수를 입력하세요.");
 		score = scanner.nextInt();
 		String year2 = "";
-		
-		
 
-		if(year1 >=1 && year1 < 5 && score <= 100 && score >=0) {
-			
-			if(year1 ==4 && score >=70) {
-			year2 = "합격";
-		
-			}else if(year1<4 && score >= 60) {
-			year2 = "합격";
-			
-			}else {
-			year2 = "불합격";
+		if (year1 >= 1 && year1 < 5 && score <= 100 && score >= 0) {
+
+			if (year1 == 4 && score >= 70) {
+				year2 = "합격";
+
+			} else if (year1 < 4 && score >= 60) {
+				year2 = "합격";
+
+			} else {
+				year2 = "불합격";
 			}
-			System.out.println("귀하는 " + year2+  "입니다.");
-	
-		}else {
-		System.out.println("나띵.");
-		
+			System.out.println("귀하는 " + year2 + "입니다.");
+
+		} else {
+			System.out.println("나띵.");
+
 		}
+
 	}
 }
---
+++ src/ch04_if/Ex08_IfExample.java
@@ -0,0 +1,46 @@
+package ch04_if;
+
+import java.util.Scanner;
+
+public class Ex08_IfExample {
+
+	public static void main(String[] args) {
+		/**
+		 * 서로 다른 정수 3개를 입력받아서 가장 큰 정수 출력하기 (같은 숫자는 입력하지 않는다고 가정) 입력 예 첫번째숫자: 두번째숫자:
+		 * 세번째숫자:
+		 * 
+		 * 출력 예 가장 큰 숫자는 00입니다.
+		 */
+
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("첫 번째 숫자를 입력하세요.");
+		num1 = scanner.nextInt();
+		System.out.println("두 번째 숫자를 입력하세요.");
+		num2 = scanner.nextInt();
+		System.out.println("세 번째 숫자를 입력하세요.");
+		num3 = scanner.nextInt();
+		int number = 0;
+
+		if (num1 > num2) {
+			if (num1 > num3) {
+				number = num1;
+			} else {
+				number = num3;
+			}
+		} else if (num2 > num1) {
+			if (num2 > num3) {
+				number = num2;
+			} else  {
+				number = num3;
+			}
+		}
+		System.out.println("The biggest number is " + number);
+
+	}
+
+}
