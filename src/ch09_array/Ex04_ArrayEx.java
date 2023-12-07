55a77e1b13dab3e8e6ab1c8595534713f3cf0f5e
linebutcher <dlxodid1115@gmail.com>
2023-12-07 16:02:55 +0900

    20231207(Array)_1

---
+++ src/ch09_array/Ex04_ArrayEx.java
@@ -0,0 +1,24 @@
+package ch09_array;
+
+import java.util.Scanner;
+
+public class Ex04_ArrayEx {
+    public static void main(String[] args) {
+        int num[] = {5, 2, 1, 3, 4, 7, 6, 9, 10, 8};
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            int num1 = scanner.nextInt();
+            int i = 0;
+            for (i = 0; i < num.length; i++) {
+                if (num[i] == num1) {
+
+                    System.out.println(i);
+                }
+
+            }
+
+        }
+    }
+}
