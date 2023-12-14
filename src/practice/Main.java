8cc59b5780562defb9e5ebaa6c4bdb9cdc8ceff1
linebutcher <dlxodid1115@gmail.com>
2023-12-14 16:55:19 +0900

    20231214(ArrayList)_8

---
+++ src/practice/Main.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //준아 아이디 입력
+        System.out.println("아이디를 입력하세요");
+        String id = scanner.next();
+        System.out.println("id = " + id + " ??!");
+
+
+    }
+}
